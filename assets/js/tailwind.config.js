09b31bf39f1c63dabfb3c44e49a23f368399aa1e
Han-Teng Liao <hanteng@gmail.com>
2024-11-27 11:56:54 +0800

    tailwind customize assets\css and js
    tailwind customize assets\css and js

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,16 @@
+// https://tailwindcss.com/docs/font-size Customizing your theme
+module.exports = {
+  theme: {
+    fontSize: {
+      xs: '0.5rem',
+      sm: '0.75rem',
+      base: '0.85rem',
+      xl: '1rem',
+      '2xl': '1.25rem',
+      '3xl': '1.563rem',
+      '4xl': '1.953rem',
+      '5xl': '2.441rem',
+      '6xl': '3.052rem',
+    }
+  }
+}
